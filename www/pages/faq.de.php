a83535aad1ada957338439cd5a26cf46bddbd2bf
draconigen <draconigen@gmail.com>
2024-07-25 13:30:50 +0200

    feat: :rocket: add faq page

---
+++ www/pages/faq.de.php
@@ -0,0 +1,28 @@
+<section>
+    <h1>Häufig gestellte Fragen</h1>
+
+    <h2>F: Wieviel kostet so ein Kostüm und wie setzt sich der Preis zusammen?</h2>
+    <p>A: Der Preis errechnet sich aus Materialkosten und Arbeitszeit. Je aufwändiger ein Fursuit ist desto mehr wird es am Ende kosten. Du kannst dir einen ungefähren Eindruck auf der  <a href="de/prices">Preise- Seite</a> machen.</p>
+
+    <h2>F: Wie bestelle Ich?</h2>
+    <p>A: Wenn ich einen Slot für Commissions geöffnet habe (Was für gewöhnlich auf der <a href="de/home">Startseite</a> angezeigt wird), klicke dich einfach durch den<a href="de/prices">Preisrechner</a>und sende mir das Kontaktformular Ich werde alsbald möglich antworten. Ich nehme mir jedoch das recht herraus auch Anfragen abzulehnen.</p>
+    
+    <h2>F: Wann öffnest du für Comissions?</h2>
+    <p>A: Für gewöhnlich immer dann wenn ich fertig mit einem Projekt bin und am Beginn eines Monats- Verfolg einfach meinen <a href="https://twitter.com/LunoWroo">Twitter Feed</a> oder <a href="http://www.furaffinity.net/user/lunowroo/">FurAffinity</a>, denn dort werde ich es vorher ankündigen.</p>
+    
+    <img src="img/pages/faq/faqluno.png" alt="" class="uk-float-right uk-visible@s" />
+    <h2>F: Wie kann ich bezahlen?</h2>
+    <p>A: Bei Bestellungen aus Deutschland würde ich Überweisung vorziehen. In jedem Fall kann aber auch mit PayPal bezahlt werden.</p>
+    
+    <h2>F: Was für Materialien benutzt du?</h2>
+    <p>A: Für sämtliche Fursuits benutze ich sogenanntes "Faux Fur" oder auch "Luxury Shag". Der 3D- Druck wird mit Pulverförmigen Nylon mittels SLS-Druck gefertigt</p>
+    
+    <h2>F: Warum das aufwendige und teure Druckverfahren für die Köpfe?</h2>
+    <p>A: Einfach weil ich's kann! Und weil es einige Vorteile bietet: Ich kann sehr genau an der Kundenvorgabe arbeiten und sogar eine kleine Vorschau rendern. Außerdem ist die Kopfbasis sehr leicht, hat eine sehr gute Akkustik und wird nicht ganz so heiß wie eine Resin- oder eine Schaumstoffbasis.</p>
+    
+    <h2>F: Mein Fursuit ist schmutzig geworden- wie wasche ich ihn?</h2>
+    <p>A: Hierzu habe Ich eine kleine <a href="de/care">Pflegeanleitung</a> gemacht. Wirf einfach einen Blick drauf.</p>
+    
+    <h2>F: An meinem suit ist etwas kaputt- wie kann ich es reparieren?</h2>
+    <p>A:Die Gute Nachricht zuerst: Der Komplette suit ist Handgemacht und im Grunde gibt es nichts daran was man nicht reparieren kann. In einem gewissen Rahmen gilt natürlich auch die gesetzliche Gewährleistung für Fursuits die ich hergestellt habe. In jedem Fall kannst Du mich einfach <a href="de/contact">kontaktieren</a> und wir finden eine Lösung.</p>
+</section>
---
+++ www/pages/faq.en.php
@@ -0,0 +1,28 @@
+<section>
+    <h1>Frequently Asked Questions</h1>
+
+    <h2>Q: How much does a costume cost and how do you calculate the price?</h2>
+    <p>A: The price comes together from material and working hours; the more effort I put into it, the more expensive it'll get. You'll get a rough idea on my <a href="en/prices">pricing page</a>.</p>
+
+    <h2>Q: How can I order?</h2>
+    <p>A: If I am open for comissions (that's usually advertised on the <a href="en/home">front page</a>, but you can always ask), feel free to fill out the the <a href="en/prices">price calculator</a> and submit it to me. I'll get back at you as soon as possible. Please note that I reserve the right to decline requests.</p>
+	
+	<h2>Q: When are you open for comissions?</h2>
+    <p>A: Usually as soon as I have finished my current projects - I will announce the dates on my <a href="https://twitter.com/LunoWroo">Twitter account</a> and on <a href="http://www.furaffinity.net/user/lunowroo/">FurAffinity</a>.</p>
+	
+    <img src="img/pages/faq/faqluno.png" alt="" class="uk-float-right uk-visible@s" />
+	<h2>Q: What sort of payment do you accept?</h2>
+    <p>A: If you order from Germany, I'd prefer bank transfer. Otherwise you can pay via PayPal.</p>
+	
+	<h2>Q: What kind of materials are you using?</h2>
+    <p>A: For the costumes I use high-quality faux fur and a 3D-printed nylon head base.</p>
+	
+	<h2>Q: Why do you use expansive 3D Printing for the head?</h2>
+    <p>A: Simply because of its' advantages: I can design the head base according to the customer's desire and even render a small 3D preview of how the suit will look like. Furthermore, the base is very light, provides good accoustic properties and won't heat you up as much as a foam or resin base.</p>
+	
+	<h2>Q: My Costume got dirty - How can I wash it?</h2>
+    <p>A: Please see the <a href="en/care">information page "Care Guide"</a> about what you can and shouldn't do about it.</p>
+	
+	<h2>Q: Something is broken, how do I get it fixed?</h2>
+    <p>A: Since the whole suit is handmade there's nothing one cannot repair. I'll fix things to a certain extend, depending on the nature of the issue. Please <a href="en/contact">contact</a> me on any way to work something out.</p>
+</section>
