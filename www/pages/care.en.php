0986518cb079efdcac31e50fa128103efe0a661c
draconigen <draconigen@gmail.com>
2024-07-25 13:24:15 +0200

    chore: :speech_balloon: proofread care page

--- www/pages/care.en.php
+++ www/pages/care.en.php
@@ -1,35 +1,39 @@
 <section>
-    <h1>How to Take Care Of Your Fursuit</h1>
+    <h1>How to Take Care of Your Fursuit</h1>
+    
     <h3>Before You Start Suiting...</h3>
     <p>
-    A fursuit is an exceptional type of clothing. On the one hand, it is a performance costume which has to provide good properties for acting, while on the other hand it's made out of faux fur, which works against that. The fur will make you sweat while you wear it, it will catch dust over time, and the hairs will get kinks. <br>
-    All of that is inevitable - take good care of your suit, and you will be able to enjoy it much longer.<br>
-    Use your common sense, treat it like you would treat a classic suit.
+        A fursuit is an exceptional type of clothing. On the one hand, it is a performance costume that has to provide good properties for acting, while on the other hand, it's made out of faux fur, which works against that. The fur will make you sweat while you wear it, it will catch dust over time, and the hairs will get kinks.<br>
+        All of that is inevitable - but with good care, you can enjoy your suit for a much longer time.<br>
+        Use your common sense, and treat it like you would treat a classic suit.
     </p>
 
     <img src="img/pages/care/infoluno.png" alt="" class="uk-float-right uk-hidden@s" />
 
     <h3>Put Your Suit On!</h3>
     <p>
-    It may sound like a little detail, but take your time putting on your suit and don't hurry it. Most incidents happen while being in a rush.<br>
-    It is advisable to have a second pair of hands when putting on your suit. <br>
-    An often underestimated part of a performance is the clothing under your suit. Wearing HeatGear or similar underwear can reduce the discomfort by heat and protects the costume from being soaked in sweat.<br>
+        It may sound like a small detail, but take your time putting on your suit and don't hurry. Most incidents happen when you're in a rush.<br>
+        It is advisable to have a second pair of hands when putting on your suit.<br>
+        An often underestimated part of a performance is the clothing under your suit. Wearing HeatGear or similar underwear can reduce discomfort from heat and protect the costume from being soaked in sweat.<br>
     </p>
 
     <h3>The Performance is Over - What Now?</h3>
     <p>
-    Take any brush you would use for your own hair and use it - The fur fibers tend to "memorize" kinks, but if you brush it regularly, you can reduce this effect. It's also recommended turning your suit inside out, hang it somewhere to dry and spray it with disinfectant spray. 
+        Take any brush you would use for your own hair and use it on your fursuit. The fur fibers tend to "memorize" kinks, but if you brush it regularly, you can reduce this effect. It's also recommended to turn your suit inside out, hang it somewhere to dry, and spray it with disinfectant spray.
     </p>
 
     <h3>Washing Your Suit is Inevitable</h3>
-    <p>The fursuit body is usually machine washable - turn your suit inside out, use a cold program <b>(e.g. not above 30&#xba; C)</b> and use a delicate cycle. <br>
-    <b>Do not put it into a dryer</b>, however! The dryer is too hot and will crumble the fur permanently.
-    It is recommended to wash all other parts of the suit by hand.<br>
-    <br>
-    Video Tutorial: <br>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/PfbqBD0-IYc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
+    <p>
+        The fursuit body is usually machine washable - turn your suit inside out, use a cold program <b>(e.g., not above 30&#xba; C)</b> and use a delicate cycle.<br>
+        <b>Do not put it into a dryer</b>, however! The dryer is too hot and will ruin the fur permanently.<br>
+        It is recommended to wash all other parts of the suit by hand.<br>
+        <br>
+        Video Tutorial: <br>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/PfbqBD0-IYc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </p>
 
     <h3>The Next Con Seems Years Away - How to Store the Suit</h3>
-    <p>It may be nice to put your fursuit's head on the cupboard for show-off - however that's not a good idea. The fur again is our culprit once again: it provides a huge surface for any kind of dust and having it lying around will make it catch dust like hell. If you want to enjoy your suit for a long time, put it into a plastic bag in a dark box where it is protected from dust and sunlight. Make sure you store it without any kinks. Like I said before, the fur tends to memorize this, especially over time.
+    <p>
+        It may be nice to put your fursuit's head on the cupboard for display; however, that's not a good idea. The fur is our culprit once again: it provides a huge surface for any kind of dust, and leaving it out will make it catch dust like crazy. If you want to enjoy your suit for a long time, put it into a plastic bag in a dark box where it is protected from dust and sunlight. Make sure you store it without any kinks. As I said before, the fur tends to memorize this, especially over time.
     </p>
 </section>
