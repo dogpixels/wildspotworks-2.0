515f6e8d952a29a5549ba60fd442d51504091d55
draconigen <draconigen@gmail.com>
2024-07-25 13:19:46 +0200

    chore: :speech_balloon: proofread tips page

--- www/pages/tips.de.php
+++ www/pages/tips.de.php
@@ -1,34 +1,37 @@
 <section>
-    <h1>4 Tipps um deine Fursuit Performance zu verbessern</h1>
+    <h1>4 Tipps, um deine Fursuit-Performance zu verbessern</h1>
+    
     <h3>Tipp 1: Kenn dich selbst!</h3>
     <p>
-    Einen Fursuit besitzen ist nur ein kleiner Teil der Darstellung eines Charakters. Das schönste am Fursuiten ist zweifelsohne in eine "fremde Haut" zu schlüpfen und andere Leute damit für seine eigene Fantasiefigur zu begeistern. <br>
-    Dabei gilt es vor allem keine falsche Scheu zu zeigen. Keiner weiß, wer unter dem Fell steckt und genau deswegen hast du die Möglichkeit, dich ohne Ende lächerlich zu machen, vor allem ohne Konsequenzen befürchten zu müssen- sei einfach deine Rolle.<br>
-    Sicher hast du dir schon einmal Gedanken gemacht, welche Wesenszüge deine Fursona ausmachen. Ist dein Charakter eher scheu und bewegt sich ängstlich gegenüber fremden Dingen, oder ist er neugierig und muss alles und jeden beschnüffeln? Stell dir Fragen, wie dein Charakter in speziellen Situationen reagieren würde! Es lohnt sich  konsequent seinen Stil und Alleinstellungsmerkmale zu verfolgen. Ein wenig Üben vor dem Spiegel schadet auch nicht.<br>
-    Wenn du als Fursuiter nur herumstehst, wirst du für Außenstehende eher uninteressant oder gar unheimlich aussehen. Fursuiten ist halt eine Performancekunst und keine Modenschau.<br>
+        Einen Fursuit zu besitzen ist nur ein kleiner Teil der Darstellung eines Charakters. Das Schönste am Fursuiten ist zweifelsohne, in eine "fremde Haut" zu schlüpfen und andere Leute damit für seine eigene Fantasiefigur zu begeistern. <br>
+        Dabei gilt es vor allem, keine falsche Scheu zu zeigen. Keiner weiß, wer unter dem Fell steckt, und genau deswegen hast du die Möglichkeit, dich ohne Ende lächerlich zu machen, vor allem ohne Konsequenzen befürchten zu müssen - sei einfach deine Rolle.<br>
+        Sicher hast du dir schon einmal Gedanken gemacht, welche Wesenszüge deine Fursona ausmachen. Ist dein Charakter eher scheu und bewegt sich ängstlich gegenüber fremden Dingen, oder ist er neugierig und muss alles und jeden beschnüffeln? Stell dir Fragen, wie dein Charakter in speziellen Situationen reagieren würde! Es lohnt sich, konsequent seinen Stil und Alleinstellungsmerkmale zu verfolgen. Ein wenig Übung vor dem Spiegel schadet auch nicht.<br>
+        Wenn du als Fursuiter nur herumstehst, wirst du für Außenstehende eher uninteressant oder gar unheimlich aussehen. Fursuiten ist eine Performancekunst und keine Modenschau.<br>
     </p>
 
     <h3>Tipp 2: Kenn dein Publikum!</h3>
     <p>
-    Ein enorm wichtiger Faktor deiner Performance ist dein Publikum.<br>
-    Gute Performer haben ein Gespür dafür, welche Leute sie an sprechen können und wie sie diese für sich begeistern und mit ihnen interagieren. Dabei darf man als Fursuiter Dinge tun, die einem unverkleidet nicht erlaubt wären. Einem übergroßen Tier lässt man halt eher einen Streich durchgehen als einem Menschen. Wichtig aber dabei vor allem: Feingefühl! <br>
-    Vorsicht vor Taten, die die Grenzen anderer Menschen überschreiten. Einfach jemand Wildfremden umarmen ist in vielen Fällen in Ordnung, jedoch solltest du sicher sein, dass derjenige dies nicht als zu tiefen Angriff auf seine persönliche Distanzzone interpretiert.<br>
-    Kinder sind dabei ein spezielles Publikum für sich. Du wirst auf pflegeleichte Kinder treffen, die einfach mal das große „Plüschtier“ umarmen wollen, aber auch auf solche, die aus Verunsicherung heraus Angst bekommen oder dich gar angreifen. Es ist nicht unbedingt ratsam, ein Kind zwingen zu wollen, sich auf einen Fursuiter einzulassen, wenn es das nicht will. Solltest du aber in die Situation geraten, dass du dieses Kind nicht umgehen kannst, dann kann ein einfaches In-die-Hocke-Gehen und dem Kind ein „High-Five“ anbieten, die Sache entschärfen.<br>
+        Ein enorm wichtiger Faktor deiner Performance ist dein Publikum.<br>
+        Gute Performer haben ein Gespür dafür, welche Leute sie ansprechen können und wie sie diese für sich begeistern und mit ihnen interagieren. Dabei darf man als Fursuiter Dinge tun, die einem unverkleidet nicht erlaubt wären. Einem übergroßen Tier lässt man halt eher einen Streich durchgehen als einem Menschen. Wichtig ist dabei vor allem: Feingefühl! <br>
+        Vorsicht vor Taten, die die Grenzen anderer Menschen überschreiten. Einfach jemanden Wildfremden umarmen, ist in vielen Fällen in Ordnung. Jedoch solltest du sicher sein, dass derjenige dies nicht als einen tiefen Angriff auf seine persönliche Distanzzone interpretiert.<br>
+        Kinder sind dabei ein spezielles Publikum für sich. Du wirst auf pflegeleichte Kinder treffen, die einfach mal das große „Plüschtier“ umarmen wollen, aber auch auf solche, die aus Verunsicherung heraus Angst bekommen oder dich gar angreifen. Es ist nicht unbedingt ratsam, ein Kind zwingen zu wollen, sich auf einen Fursuiter einzulassen, wenn es das nicht will. Solltest du aber in die Situation geraten, dass du dieses Kind nicht umgehen kannst, dann kann ein einfaches In-die-Hocke-Gehen und dem Kind ein „High-Five“ anbieten die Sache entschärfen.<br>
     </p>
 
     <h3>Tipp 3: Kenn deine Umgebung!</h3>
     <p>
-    Fursuiten ist eine körperlich enorm fordernde Tätigkeit. Je mehr man sich bewegt, desto anstrengender ist es natürlich. Außerdem ist man im Fursuit sensorisch stark eingeschränkt. Man beraubt sich des räumlichen Sehens, des Tastsinns und Gerüche sowie Geräusche dringen schlechter an die Sinnesorgane. Von daher sollte man sich überlegen, ob man in der Verfassung ist zu suiten, sprich sich fit fühlt und die örtlichen Gegebenheiten passen.<br>
-    Ein guter Ort zum fursuiten ist beispielsweise eine Fußgängerzone: Man hat viel Publikum und das Gelände ist meist optimal erschlossen. Wenn man im Anschluss in die nahe gelegene Einkaufspassage geht, um dort weiter zu performen, sollte man sich aber bewusst sein, dass es zu Problemen kommen kann. Die Betreiber der Einkaufspassagen sehen es unter Umständen nicht gern, wenn unangemeldet vollverkleidete Personen durch ihre Hallen laufen und werden gegebenenfalls von ihrem Hausrecht Gebrauch machen. <br>
-    Das heißt aber nicht, dass es grundsätzlich unmöglich ist. Eine nette Anfrage per Telefon oder E-Mail, im Vorfeld hilft mitunter Missverständnisse aus der Welt zu räumen.<br>
-    Auf jeden Fall ist es ratsam, vorher eine Route festzulegen, damit gegebenenfalls gefährliche Straßen oder Treppen umgangen werden können und wo du Stellen festlegst, an denen du eine Pause einlegen kannst.<br>
+        Fursuiten ist eine körperlich enorm fordernde Tätigkeit. Je mehr man sich bewegt, desto anstrengender ist es natürlich. Außerdem ist man im Fursuit sensorisch stark eingeschränkt. Man beraubt sich des räumlichen Sehens, des Tastsinns, und Gerüche sowie Geräusche dringen schlechter an die Sinnesorgane. Von daher sollte man sich überlegen, ob man in der Verfassung ist, zu suiten, sprich, sich fit fühlt und die örtlichen Gegebenheiten passen.<br>
+        Ein guter Ort zum Fursuiten ist beispielsweise eine Fußgängerzone: Man hat viel Publikum und das Gelände ist meist optimal erschlossen. Wenn man im Anschluss in die nahe gelegene Einkaufspassage geht, um dort weiter zu performen, sollte man sich aber bewusst sein, dass es zu Problemen kommen kann. Die Betreiber der Einkaufspassagen sehen es unter Umständen nicht gern, wenn unangemeldet vollverkleidete Personen durch ihre Hallen laufen und werden gegebenenfalls von ihrem Hausrecht Gebrauch machen. <br>
+        Das heißt aber nicht, dass es grundsätzlich unmöglich ist. Eine nette Anfrage per Telefon oder E-Mail im Vorfeld hilft mitunter, Missverständnisse aus der Welt zu räumen.<br>
+        Auf jeden Fall ist es ratsam, vorher eine Route festzulegen, damit gegebenenfalls gefährliche Straßen oder Treppen umgangen werden können, und festzulegen, wo du eine Pause einlegen kannst.<br>
     </p>
 
     <h3>Tipp 4: Kenn deinen Fursuit!</h3>
-    <p>Jeder Fursuit ist ein absolutes Einzelstück und sollte auch als solches betrachtet werden. Insbesondere deswegen solltest du Eigenarten und Besonderheiten deines Fursuits kennen: Wo sind die Ohren und wie sieht es aus, wenn ich sie mir zuhalte? Welche Geste hat welchen Ausdruck? <br>
-    Ebenfalls ist es wichtig zu berücksichtigen, welche Bewegungsfreiheit dir dein Suit lässt und wie lange du es darin überhaupt aushältst. Dabei sind manche Suits, baubedingt eher für Action geeignet als andere. <br>
-    Womit wir schon beim Thema sind: Ein großes Problem beim suiten ist die Hitzeentwicklung. Sogenanntes Spandex oder „Heat Gear“ kann dir nicht nur helfen kühler zu bleiben, sondern auch Schweiß auffangen und damit den Suit schützen. Zusätzlich gibt es „Cooling Vests“, die mittels spezieller Kühlakkus helfen die ganze Performance angenehmer zu machen. <br>
-    Ein weiterer Trick ist es, regelmäßig zu saunieren, denn dadurch lernt dein Körpermit höheren Temperaturen umzugehen.<br>
-    Das Wichtigste ist aber, sich nicht zu überschätzen. Nichts ist schlimmer als im Suit zusammenzubrechen, denn die Ersthelfer haben meist keine andere Wahl, als dich dann freizuschneiden. <br>
-    Vor und nach dem Suitwalk solltest du daher genug trinken und etwas salziges essen. Leichte Kopfschmerzen sind ein Warnsignal und sollten dir und deinem Suit daher Anlass zu einer Pause geben.</p>
+    <p>
+        Jeder Fursuit ist ein absolutes Einzelstück und sollte auch als solches betrachtet werden. Insbesondere deswegen solltest du die Eigenarten und Besonderheiten deines Fursuits kennen: Wo sind die Ohren, und wie sieht es aus, wenn ich sie mir zuhalte? Welche Geste hat welchen Ausdruck? <br>
+        Ebenfalls ist es wichtig zu berücksichtigen, welche Bewegungsfreiheit dir dein Suit lässt und wie lange du es darin überhaupt aushältst. Dabei sind manche Suits bauartbedingt eher für Action geeignet als andere. <br>
+        Womit wir schon beim Thema sind: Ein großes Problem beim Suiten ist die Hitzeentwicklung. Sogenanntes Spandex oder „Heat Gear“ kann dir nicht nur helfen, kühler zu bleiben, sondern auch Schweiß auffangen und damit den Suit schützen. Zusätzlich gibt es „Cooling Vests“, die mittels spezieller Kühlakkus helfen, die ganze Performance angenehmer zu machen. <br>
+        Ein weiterer Trick ist es, regelmäßig zu saunieren, denn dadurch lernt dein Körper, mit höheren Temperaturen umzugehen.<br>
+        Das Wichtigste ist aber, sich nicht zu überschätzen. Nichts ist schlimmer, als im Suit zusammenzubrechen, denn die Ersthelfer haben meist keine andere Wahl, als dich dann freizuschneiden. <br>
+        Vor und nach dem Suitwalk solltest du daher genug trinken und etwas Salziges essen. Leichte Kopfschmerzen sind ein Warnsignal und sollten dir Anlass zu einer Pause geben.
+    </p>
 </section>
