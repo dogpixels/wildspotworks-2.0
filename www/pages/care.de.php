9c6e9ac745481608297cdd04bdb55744fbe71c4f
draconigen <draconigen@gmail.com>
2024-07-25 13:22:27 +0200

    feat: :rocket: add care page

---
+++ www/pages/care.de.php
@@ -0,0 +1,38 @@
+<section>
+    <h1>Die Pflege des Fursuits</h1>
+    <h3>Bevor du anfängst...</h3>
+    <p>
+    Ein Fursuit ist ein recht spezielles Kleidungsstück. Einerseits muss es einer hohen Beanspruchung standhalten, während man damit herumalbert. Andererseits besteht es jedoch zu einem guten Teil aus empfindlichem Kunstfell. Genau dieses Kunstfell macht die Sache etwas zweischneidig. Es wird dafür sorgen, dass du während deiner Performance schwitzt, es wird Staub geradezu magisch anziehen und seine Fasern werden knicken.<br>
+    Aber all diese Dinge lassen sich verhindern, wenn man gut damit umgeht.<br>
+    Eigentlich ist es ganz einfach: Behandle deinen Fursuit genauso wie du einen klassischen Anzug behandeln würdest und benutze deinen gesunden Menschenverstand.
+    </p>
+
+    <img src="img/pages/care/infoluno.png" alt="" class="uk-float-right uk-hidden@s" />
+
+    <h3>Zieh den Suit an!</h3>
+    <p>
+    Es klingt zuerst vielleicht banal, aber nimm dir Zeit, wenn du in den suit schlüpfst. Meiner Erfahrung nach passieren die meisten Schäden an Suits wenn man ihn übereilt anziehen möchte.<br>
+    Am besten hast du sogar jemanden, der dir hilft, ihn anzuziehen. <br>
+    Übrigens: ein oft unterschätzter Teil der Performance ist die Kleidung unter deinem Suit. Besorge dir am besten sogenannte "HeatGear" oder ähnliche Funktionsunterwäsche, die dir hilft, kühl zu bleiben und gleichzeitig deinen Schweiß aufnimmt.<br>
+    </p>
+
+    <h3>Die Show ist vorbei- was nun?</h3>
+    <p>
+    Nimm als Erstes eine Haarbürste und kämme den Fursuit - die Fellfasern tendieren dazu, sich Knicke zu merken, aber wenn du ihn regelmäßig kämmst wirst du den Effekt reduzieren können. Außerdem empfehle ich dir, den Suit auf links zu krempeln und ihn von innen zu desinfizieren. 
+    </p>
+
+    <h3>Die unvermeidbare Wäsche</h3>
+    <p>Ein Fursuitbody ist für gewöhnlich waschmaschinenenfest- krempel ihn auf links und wasch ihn im Schonwaschprogramm bei kaltem Wasser <b>(nicht über 30&#xba; C)</b>. <br>
+    <b>Aber tu ihn nicht in den Trockner</b>! Der trockner ist zu heiß für die Kunstfasern und würde den Suit geradezu vernichten.
+    Bei den anderen Teilen des Suits empfehle ich Handwäsche.<br>
+    Der Suit selbst wird bei der Wäsche eine enorme Menge an Wasser aufsaugen und daher auch enorm schwer werden. Bedenke das wenn du ihn irgendwo aufhängen möchtest. Wichtig ist auch hier, den Trocknungsprozess zu überwachen und die einzelnen Fasern schon in die richtige Richtung zu kämmen, sobald er aus der Maschine kommt. Dies solltest du nochmal tun, sobald er abgetropft ist und noch einmal sobald er trocken ist.<br>
+    Wie genau er zu trocknen ist, ist ein wenig umstritten. Ich persönlich bevorzuge ihn irgendwo aufzuhängen, damit mir die Schwerkraft einen Teil der Kämmarbeit abnimmt, aber im Grunde kann der Suit auch liegend getrocknet werden. Wichtig ist dabei nur, dass die Richtung des Fells erhalten bleibt.<br>
+    <br>
+    Video Tutorial: <br>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/PfbqBD0-IYc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </p>
+
+    <h3>Es ist noch ewig bis zur nächsten Con- wie lagere ich meinen Suit?</h3>
+    <p>Es mag cool aussehen, wenn man seinen Fursuit in der Wohnung ausstellt, sodass ihn Jeder sehen kann, aber es ist leider keine gute Idee. Und es liegt wieder einmal am Fell: Denn dieses bietet eine riesige Oberfläche, die sämtlichen Staub der Wohnung einfängt. Wenn du lange Zeit Freude an deinem Suit haben willst, solltest du ihn vor Staub und Sonnenlicht geschützt lagern- in einer Kiste oder im Schrank. Beachte dabei dass du ihn möglichst ohne Knicke lagerst, wie bereits gesagt, dass Fell merkt sich schlechte Behandlung...
+    </p>
+</section>
