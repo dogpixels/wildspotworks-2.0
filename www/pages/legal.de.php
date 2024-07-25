c35cd9450620e4c0eb1fac03f34408d81e19f6c5
draconigen <draconigen@gmail.com>
2024-07-25 13:55:47 +0200

    feat: :rocket: add legal page

---
+++ www/pages/legal.de.php
@@ -0,0 +1,30 @@
+<section>
+    <h1>Impressum</h1>
+
+    <p>Verantwortlich für diesen Webauftritt:</p>
+
+    <p>
+        <strong>
+            Tobias Lück<br />
+            Bahnhofstr. 66<br />
+            04934 Hohenleipisch<br />
+        </strong>
+    </p>
+
+    <p>
+        <strong>E-Mail: </strong>mail@wildspotworks.com
+    </p>
+
+    <p>
+        <strong>Tel.: </strong>+49 173 1482 095
+    </p>
+
+</section>
+
+<section>
+    <h2>Drittsoftware</h2>
+    <ul>
+        <li><a href="https://getuikit.com/" target="_blank">UIKit &copy; YOOtheme</a> | <a href="https://github.com/uikit/uikit/blob/develop/LICENSE.md">MIT-Lizenz</a></li>
+        <li><a href="https://www.patreon.com/posts/free-font-from-99898740" target="_blank">Zhurzh's Schriftart</a></li>
+    </ul>
+</section>
---
+++ www/pages/legal.en.php
@@ -0,0 +1,30 @@
+<section>
+    <h1>Legal Info / Impressum</h1>
+
+    <p>Responsible for this website:</p>
+
+    <p>
+        <strong>
+            Tobias Lück<br />
+            Bahnhofstr. 66<br />
+            04934 Hohenleipisch<br />
+        </strong>
+    </p>
+
+    <p>
+        <strong>E-Mail: </strong>mail@wildspotworks.com
+    </p>
+
+    <p>
+        <strong>Tel.: </strong>+49 173 1482 095
+    </p>
+
+</section>
+
+<section>
+    <h2>Attributions</h2>
+    <ul>
+        <li><a href="https://getuikit.com/" target="_blank">UIKit &copy; YOOtheme</a> | <a href="https://github.com/uikit/uikit/blob/develop/LICENSE.md">MIT License</a></li>
+        <li><a href="https://www.patreon.com/posts/free-font-from-99898740" target="_blank">Zhurzh's font</a></li>
+    </ul>
+</section>
