bb317284dbe31a7a41135349d912bf866277c0f5
draconigen <draconigen@gmail.com>
2024-07-25 13:32:09 +0200

    chore: :speech_balloon: proofread faq page

--- www/pages/faq.de.php
+++ www/pages/faq.de.php
@@ -1,28 +1,29 @@
 <section>
     <h1>Häufig gestellte Fragen</h1>
 
-    <h2>F: Wieviel kostet so ein Kostüm und wie setzt sich der Preis zusammen?</h2>
-    <p>A: Der Preis errechnet sich aus Materialkosten und Arbeitszeit. Je aufwändiger ein Fursuit ist desto mehr wird es am Ende kosten. Du kannst dir einen ungefähren Eindruck auf der  <a href="de/prices">Preise- Seite</a> machen.</p>
+    <h2>F: Wie viel kostet so ein Kostüm und wie setzt sich der Preis zusammen?</h2>
+    <p>A: Der Preis errechnet sich aus Materialkosten und Arbeitszeit. Je aufwändiger ein Fursuit ist, desto mehr wird er am Ende kosten. Du kannst dir einen ungefähren Eindruck auf der <a href="de/prices">Preise-Seite</a> machen.</p>
 
-    <h2>F: Wie bestelle Ich?</h2>
-    <p>A: Wenn ich einen Slot für Commissions geöffnet habe (Was für gewöhnlich auf der <a href="de/home">Startseite</a> angezeigt wird), klicke dich einfach durch den<a href="de/prices">Preisrechner</a>und sende mir das Kontaktformular Ich werde alsbald möglich antworten. Ich nehme mir jedoch das recht herraus auch Anfragen abzulehnen.</p>
+    <h2>F: Wie bestelle ich?</h2>
+    <p>A: Wenn ich einen Slot für Commissions geöffnet habe (was für gewöhnlich auf der <a href="de/home">Startseite</a> angezeigt wird), klicke dich einfach durch den <a href="de/prices">Preisrechner</a> und sende mir das Kontaktformular. Ich werde so bald wie möglich antworten. Ich nehme mir jedoch das Recht heraus, auch Anfragen abzulehnen.</p>
     
-    <h2>F: Wann öffnest du für Comissions?</h2>
-    <p>A: Für gewöhnlich immer dann wenn ich fertig mit einem Projekt bin und am Beginn eines Monats- Verfolg einfach meinen <a href="https://twitter.com/LunoWroo">Twitter Feed</a> oder <a href="http://www.furaffinity.net/user/lunowroo/">FurAffinity</a>, denn dort werde ich es vorher ankündigen.</p>
+    <h2>F: Wann öffnest du für Commissions?</h2>
+    <p>A: Für gewöhnlich immer dann, wenn ich mit einem Projekt fertig bin, und am Beginn eines Monats. Verfolge einfach meinen <a href="https://twitter.com/LunoWroo">Twitter Feed</a> oder <a href="http://www.furaffinity.net/user/lunowroo/">FurAffinity</a>, denn dort werde ich es vorher ankündigen.</p>
     
-    <img src="img/pages/faq/faqluno.png" alt="" class="uk-float-right uk-visible@s" />
+    <img src="img/pages/faq/faqluno.png" alt="" class="uk-float-right uk-hidden@s" />
+
     <h2>F: Wie kann ich bezahlen?</h2>
     <p>A: Bei Bestellungen aus Deutschland würde ich Überweisung vorziehen. In jedem Fall kann aber auch mit PayPal bezahlt werden.</p>
     
     <h2>F: Was für Materialien benutzt du?</h2>
-    <p>A: Für sämtliche Fursuits benutze ich sogenanntes "Faux Fur" oder auch "Luxury Shag". Der 3D- Druck wird mit Pulverförmigen Nylon mittels SLS-Druck gefertigt</p>
+    <p>A: Für sämtliche Fursuits benutze ich sogenanntes "Faux Fur" oder auch "Luxury Shag". Der 3D-Druck wird mit pulverförmigem Nylon mittels SLS-Druck gefertigt.</p>
     
     <h2>F: Warum das aufwendige und teure Druckverfahren für die Köpfe?</h2>
-    <p>A: Einfach weil ich's kann! Und weil es einige Vorteile bietet: Ich kann sehr genau an der Kundenvorgabe arbeiten und sogar eine kleine Vorschau rendern. Außerdem ist die Kopfbasis sehr leicht, hat eine sehr gute Akkustik und wird nicht ganz so heiß wie eine Resin- oder eine Schaumstoffbasis.</p>
+    <p>A: Einfach, weil ich's kann! Und weil es einige Vorteile bietet: Ich kann sehr genau an der Kundenvorgabe arbeiten und sogar eine kleine Vorschau rendern. Außerdem ist die Kopfbasis sehr leicht, hat eine sehr gute Akustik und wird nicht ganz so heiß wie eine Resin- oder eine Schaumstoffbasis.</p>
     
-    <h2>F: Mein Fursuit ist schmutzig geworden- wie wasche ich ihn?</h2>
-    <p>A: Hierzu habe Ich eine kleine <a href="de/care">Pflegeanleitung</a> gemacht. Wirf einfach einen Blick drauf.</p>
+    <h2>F: Mein Fursuit ist schmutzig geworden - wie wasche ich ihn?</h2>
+    <p>A: Hierzu habe ich eine kleine <a href="de/care">Pflegeanleitung</a> gemacht. Wirf einfach einen Blick darauf.</p>
     
-    <h2>F: An meinem suit ist etwas kaputt- wie kann ich es reparieren?</h2>
-    <p>A:Die Gute Nachricht zuerst: Der Komplette suit ist Handgemacht und im Grunde gibt es nichts daran was man nicht reparieren kann. In einem gewissen Rahmen gilt natürlich auch die gesetzliche Gewährleistung für Fursuits die ich hergestellt habe. In jedem Fall kannst Du mich einfach <a href="de/contact">kontaktieren</a> und wir finden eine Lösung.</p>
+    <h2>F: An meinem Suit ist etwas kaputt - wie kann ich es reparieren?</h2>
+    <p>A: Die gute Nachricht zuerst: Der komplette Suit ist handgemacht und im Grunde gibt es nichts daran, was man nicht reparieren kann. In einem gewissen Rahmen gilt natürlich auch die gesetzliche Gewährleistung für Fursuits, die ich hergestellt habe. In jedem Fall kannst du mich einfach <a href="de/contact">kontaktieren</a> und wir finden eine Lösung.</p>
 </section>
