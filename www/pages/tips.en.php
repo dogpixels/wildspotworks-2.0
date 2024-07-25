02d5401dd948943361ba1cc2d33c913860dc4dad
draconigen <draconigen@gmail.com>
2024-07-25 13:16:45 +0200

    feat: :rocket: add tips page

---
+++ www/pages/tips.en.php
@@ -0,0 +1,42 @@
+<section>
+    <h1>Improve your Fursuit Performance with four hints</h1>
+    <h3>Hint 1: Know Yourself!</h3>
+    <p>
+    Just owning a fursuit is the easiest part of portraying your character. One of the most enjoyable things of fursuiting is, without a doubt, being someone else and fascinate others with your performance. <br>
+    Very important: don't be shy! Nobody knows who is beneath the fur, giving you the opportunity to make a complete fool of yourself without any consequences.<br>
+    Certainly you've thought about your fursona's characterization. Is it rather shy and does it move anxiously towards strangers? Or is it curious and sniffs everything? Ask yourself how your character acts in different situations. It is well worth practicing a style and unique features. Acting in front of a mirror won't hurt either.<br>
+    Standing around still in a fursuit will make you look boring, at the least. <br>
+    Fursuiting is an art of performance, not a fashion show.
+    </p>
+
+    <h3>Hint 2: Know Your Audience!</h3>
+    <p>
+    A tremendously important factor in your performance is your audience. Good performers have a sense of how people will react when interacting with them. Your audience enables you to perform things that you wouldn't be able to pull off otherwise.<br>
+    An oversized animal is more easily accepted when playing tricks on strangers than any human. It is still important to be sensitive, though!<br>
+    Beware of deeds that cross personal boundaries of people. Simply hugging strangers is alright in most cases, however, you should be sure that one does not take it as an assault on his personal space.<br>
+    Children are a special kind of audience. <br>
+    You will meet children who are easy to interact with, who just want to hug the "big plush animal”, but also those who are afraid of you. <br>
+    Those may tend to cry or even attack you out of fear. Don't enforce too much interaction on them, rather just hold your hand up and wait for them to come and give you a high-five. <br>
+    This is one of the most common gestures and can help you solve difficult situations.
+    </p>
+
+    <h3>Hint 3: Know Your Environment!</h3>
+    <p>
+    Fursuiting is a physically enormously demanding activity. The more you move around, the more exhausting it gets. Furthermore, you will limit you senses a lot by putting on a fursuit. That means you will deprive yourself of spatial vision, the sense of touch and sounds will dampen. Evaluate beforehand whether you are in the right condition for suiting and if the environment is “suitable” (badum-tss!).<br>
+    For example, a good place for fursuiting are pedestrian zones. You have a lot of audience and the terrain is optimally developed. Watch your step, however, when continuing your performance in the nearest shopping mall, however:<br>
+    shopping malls are private property, and its operators may get nervous about fully covered persons walking through their halls without permission. You can get into serious trouble!<br>
+    Instead, send a friendly request per mail and ask for permission. That way, mall security and owners are informed and might even be happy about your performance.<br>
+    In any case it is highly advisable to plan a route beforehand so that dangerous roads or staircases can be bypassed. It also helps you identify spots where you can take a break in emergency situations.
+    </p>
+
+    <h3>Hint 4: Know Your Fursuit!</h3>
+    <p>
+    Each fursuit is a completely unique piece, with unique features and specialties. Therefore you should learn its characteristics. Like: where are the ears of my suit and how does it look when I hold them? Which gesture will generate which expression? <br>
+    It is also important to know which freedom of movement your suit provides and how long you can suit before getting uncomfortable. Some suits are likely better for action than others.<br>
+    Furthermore: A huge problem while suiting is the heat. <br>
+    So-called “Spandex” or “HeatGear” can help you reduce temperature and also protect your suit from sweat. In addition, you could buy a “Cooling Vest” which contain special inserts to keep you cool.<br>
+    Another trick is to sauna regularly. This helps your body to get used to higher temperatures.<br>
+    But the most important thing is not to exaggerate. <br>
+    Nothing is worse than passing out in suit, because, the ambulance will cut you out of it if necessary. It is advisable to drink enough and eat something salty before suiting. Light headaches are a warning signal and therefore should give you a reason to take a break and rest a bit.
+    </p>
+</section>
