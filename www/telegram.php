bcbcd397d43ecc77a75250f19ab8a73758e40bfb
draconigen <draconigen@gmail.com>
2024-07-31 21:20:49 +0200

    feat: :sparkles: implement contact form

--- www/telegram.php
+++ www/telegram.php
@@ -1,51 +1,87 @@
 <?php declare(strict_types=1);
 
-// prepare data
+// configure endpoint
+$config = [
+    'bot_api_token' => "", // 
+    'target_userid' => "", // 
+];
+
+// init data structures
 $input = ['name' => '', 'email' => '', 'telegram' => '', 'message' => ''];
-$formDisabled = false;
+// track submission success and errors
+$success = false;
+$errors = [];
 
 // sanitize input
 if (isset($_POST['name']))
-    $input['name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    $input['name'] = htmlspecialchars($_POST['name']);
 if (isset($_POST['email']))
-    $input['name'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $input['email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 if (isset($_POST['telegram']))
-    $input['telegram'] = filter_input(INPUT_POST, 'telegram', FILTER_SANITIZE_STRING);
+    $input['telegram'] = htmlspecialchars($_POST['telegram']);
 if (isset($_POST['message']))
-    $input['message'] = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+    $input['message'] = htmlspecialchars($_POST['message']);
 
-// send
-if (isset($_POST['submit'])) {
-    $formDisabled = true;
-}
-?>
+// submit on POST
+if (!empty($_POST)) {
+    // check for empty inputs
+    if (empty($input['name']))
+        $errors['name'] = true;
+    if (empty($input['email']) && empty($input['telegram']))
+        $errors['contact'] = true;
+    if (empty($input['message']))
+        $errors['message'] = true;
 
+    if (empty($errors)) {
+        { // prepare implodable array for optional contact strings
+            $contacts = [];
 
-<?php
+            if (!empty($input['email'])) {
+                $contacts[] = $input['email'];
+            }
 
-function report(array $telegram, string $message): void
-{
-    print($message);
+            // form correct telegram username from user input
+            if (!empty($input['telegram'])) {
+                // if user provided https://t.me/username or https://t.me/@username format, strip the http part
+                if (str_starts_with($input['telegram'], 'http')) {
+                    $input['telegram'] = end(explode('/', $input['telegram']));
+                }
+                // ensure that a @ symbol precedes the username in any case
+                $contacts[] = str_starts_with($input['telegram'], '@')? $input['telegram'] : '@' . $input['telegram'];
+            }
+        }
 
-    $data = [
-        'chat_id' => $telegram['admin'],
-        'text' => $message,
-        'parse_mode' => 'MarkdownV2'
-    ];
+        { // prepare telegram api payload
+            $payload = [
+                'chat_id' => $config['target_userid'],
+                'text' => sprintf(
+                    "%s \\(%s\\):\n\n```\n%s```",
+                    filter_tg_markdown($input['name']),
+                    filter_tg_markdown(implode(', ', $contacts)),
+                    htmlspecialchars_decode($input['message'])
+                ),
+                'parse_mode' => 'MarkdownV2'
+            ];
+        }
 
-    $curl = curl_init($telegram['api']);
+        { // init & configure curl request
+            $curl = curl_init("https://api.telegram.org/bot{$config['bot_api_token']}/sendMessage");
 
-    curl_setopt_array($curl, [
-            CURLOPT_POST => true,                           // set post request method
-            CURLOPT_POSTFIELDS => http_build_query($data),  // attach url-encoded post data
-            CURLOPT_RETURNTRANSFER => true                  // return response, rather than printing it to stdout
-        ]
-    );
+            curl_setopt_array($curl, [
+                    CURLOPT_POST => true,                             // set post request method
+                    CURLOPT_POSTFIELDS => http_build_query($payload), // attach url-encoded post data
+                    CURLOPT_RETURNTRANSFER => true                    // return response, rather than printing it to stdout
+                ]
+            );
+        }
 
-    $response = curl_exec($curl);
-
-    if (!json_decode($response)->ok)
-    {
-        print("\nAdditionally to the error above, telegram error reporting failed with the following reponse from Telegram Bot API:\n{$response}");
+        { // exec api call & eval response
+            $response = curl_exec($curl);
+            $success = json_decode($response)->ok;
+        }
     }
+}
+
+function filter_tg_markdown(string $subject): string {
+    return preg_replace('/([_\*\[\]\(\)~`>#\+\-=|{}.!])/', '\\\\$1', $subject);
 }
